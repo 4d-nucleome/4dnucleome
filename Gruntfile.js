1847f72f056476f898450945eea35275621040d4
Forrest Tanaka <forrest@forrest-tanaka.com>
2015-01-16 16:29:42 -0800

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+'use strict';
+
+    grunt.initConfig({
+        compass: {
+            dev: {
+                options: {
+                    httpPath: '/',
+                    basePath: '.',
+                    cssDir: 'css',
+                    sassDir: 'sass',
+                    imagesDir: 'img',
+                    javascriptsDir: 'js',
+                    outputStyle: 'expanded',
+                    relativeAssets: false,
+                    noLineComments: false
+                }
+            }
+        },
+        watch: {
+            compass: {
+                files: [
+                    'sass/**/*.scss'
+                ],
+                tasks: ['compass']
+            },
+            livereload: {
+                // Browser live reloading
+                // https://github.com/gruntjs/grunt-contrib-watch#live-reloading
+                options: {
+                    livereload: true
+                },
+                files: [
+                    'css/*.css',
+                    '*.html'
+                ]
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Register tasks
+    grunt.registerTask('default', [
+        'compass'
+    ]);
+    grunt.registerTask('dev', [
+        'watch'
+    ]);
+
+};
---
+++ README.md
@@ -0,0 +1,13 @@
+# 4d Nucleome Portal
+
+This is currently a single introduction page to the 4D Nucleome Portal. To build this site:
+
+```
+sudo npm install
+```
+
+This installs the build tools. You can then build the required CSS files from the SCSS files with:
+
+```
+grunt
+```
